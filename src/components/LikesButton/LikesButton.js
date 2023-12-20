d5145cf027b25edf7863fb6fcac87d4554fd81a9
Nate Paradiso <ipfcolo05@gmail.com>
2023-12-19 16:20:33 -0800

    modified likes button

--- src/components/LikesButton/LikesButton.js
+++ src/components/LikesButton/LikesButton.js
@@ -1,26 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./LikesButton.scss";
 import thumbsUp from "../../assets/images/thumbsup.png";
 
 export const LikesButton = () => {
-  const [likes, setLikes] = useState(1);
-  const [isClicked, setIsClicked] = useState(false);
+  const [likes, setLikes] = useState("");
 
   const handleClick = () => {
-    if (isClicked) {
-      setLikes(likes + 1);
-    } else {
-      setIsClicked(!isClicked);
-    }
+    setLikes(likes === "" ? 1 : likes + 1);
   };
 
   return (
     <>
       <button className="likes__button" onClick={handleClick}>
         <span className="likes__button-counter">
-          {" "}
           <img className="likes__button-thumb" src={thumbsUp} alt="thumbs up icon" />
-          {`${likes}`}
+          {likes !== "" ? `${likes}` : ""}
         </span>
       </button>
     </>
