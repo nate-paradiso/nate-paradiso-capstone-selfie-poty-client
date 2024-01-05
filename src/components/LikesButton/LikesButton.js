b7348c801e629e2feed710da38cec05ff4c46c1c
Nate Paradiso <ipfcolo05@gmail.com>
2024-01-04 20:50:26 -0800

    working like button to mysql

--- src/components/LikesButton/LikesButton.js
+++ src/components/LikesButton/LikesButton.js
@@ -1,12 +1,19 @@
-import { useState } from "react";
 import "./LikesButton.scss";
 import thumbsUp from "../../assets/images/thumbsup.png";
+import axios from "axios";
 
-export const LikesButton = () => {
-  const [likes, setLikes] = useState("");
+export const LikesButton = ({ imageId, likes, handleUpdateLikes }) => {
+  const handleClick = async () => {
+    try {
+      const response = await axios.patch(`${process.env.REACT_APP_BACKEND_URL}/likes`, { imageId });
 
-  const handleClick = () => {
-    setLikes(likes === "" ? 1 : likes + 1);
+      if (response.status !== 200) {
+        throw new Error("Failed to update likes");
+      }
+      handleUpdateLikes(imageId, response.data.updatedLikes);
+    } catch (error) {
+      console.error("Error updating likes:", error.message);
+    }
   };
 
   return (
@@ -14,7 +21,7 @@ export const LikesButton = () => {
       <button className="likes__button" onClick={handleClick}>
         <span className="likes__button-counter">
           <img className="likes__button-thumb" src={thumbsUp} alt="thumbs up icon" />
-          {likes !== "" ? `${likes}` : ""}
+          {likes !== 0 ? `${likes}` : ""}
         </span>
       </button>
     </>
